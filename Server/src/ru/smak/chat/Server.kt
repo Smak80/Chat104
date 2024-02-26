f467e2454d0c3287ea6bd77b0d2be204611e20e2
Sergey Makletsov <smaklets@kpfu.ru>
2024-02-26 09:44:40 +0300

    Чат. Первая версия

--- Server/src/ru/smak/chat/Server.kt
+++ Server/src/ru/smak/chat/Server.kt
@@ -1,4 +1,26 @@
 package ru.smak.chat
 
-class Server {
+import java.net.ServerSocket
+
+class Server(port: Int) {
+    private val serverSocket = ServerSocket(port)
+    private var stop: Boolean = false
+
+    fun start(){
+        stop = false
+        try {
+            while (!stop) {
+                val socket = serverSocket.accept()
+            }
+        } catch (e: Throwable){
+            println("Произошла непредвиденная ошибка: ${e.message}")
+        }
+        finally {
+            serverSocket.close()
+        }
+    }
+
+    fun stop(){
+        stop = true
+    }
 }
